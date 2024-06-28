ede9de52908ab145b5f130b8b54940f6b6ac3512
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-06-28 10:39:05 -0700

    Added website with individual index.js file for alert scripting. The alert takes 3 separate variables calculated 3 separate ways.

---
+++ index.js
@@ -0,0 +1,15 @@
+//pop up message that will be displayed regardless of the number
+const popupMessage = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`;
+
+//first number will add modulo to 10
+const firstNum = 40 % 30;
+
+//second number will subtract down to 40
+
+const secondNum = 45 - 5;
+
+//third number will add to 39
+
+const thirdNum = 12 + 27;
+
+alert(`${popupMessage} ${firstNum} - ${secondNum} - ${thirdNum}`);
